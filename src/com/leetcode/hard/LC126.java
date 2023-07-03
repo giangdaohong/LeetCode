e019f78fa63d10813d2ede85447246d98413bb4d
Dao Hong Giang <daohonggiangak8@gmail.com>
2023-07-03 09:47:53 +0700

    Update code

--- src/com/leetcode/hard/LC126.java
+++ src/com/leetcode/hard/LC126.java
@@ -1,13 +1,17 @@
 package com.leetcode.hard;
+
 public class LC126 {
     public static void main(String[] args) {
         String version = System.getProperty("java.version");
         System.out.println(version);
-    if(version.startsWith("1.")) {
-        version = version.substring(2, 3);
-    } else {
-        int dot = version.indexOf(".");
-        if(dot != -1) { version = version.substring(0, dot); }
-    }  System.out.println(version);
+        if (version.startsWith("1.")) {
+            version = version.substring(2, 3);
+        } else {
+            int dot = version.indexOf(".");
+            if (dot != -1) {
+                version = version.substring(0, dot);
+            }
+        }
+        System.out.println(version);
     }
 }
